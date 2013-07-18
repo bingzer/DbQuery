dc3b77d9d2ed907c96b8ecd9507005773cc05412
bingzer <ricky@bingzer.com>
2013-07-18 13:07:41 -0400

    Fixed project structure again..
    Android Studio kicking my arse

--- src/main/gen/com/bingzer/android/dbquery/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.bingzer.android.dbquery;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}
