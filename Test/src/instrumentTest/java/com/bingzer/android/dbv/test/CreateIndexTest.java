f1fbe478e0483bf5c159956c69ecb762a1925e8b
bingzer <ricky@bingzer.com>
2013-08-12 22:04:35 -0400

    Test on "CREATE INDEX"

---
+++ Test/src/instrumentTest/java/com/bingzer/android/dbv/test/CreateIndexTest.java
@@ -0,0 +1,41 @@
+package com.bingzer.android.dbv.test;
+
+import android.content.Context;
+import android.test.AndroidTestCase;
+
+import com.bingzer.android.dbv.DbQuery;
+import com.bingzer.android.dbv.IDatabase;
+import com.bingzer.android.dbv.sqlite.SQLiteBuilder;
+
+/**
+ * Created by Ricky Tobing on 8/12/13.
+ */
+public class CreateIndexTest extends AndroidTestCase {
+
+    IDatabase db;
+    @Override
+    public void setUp(){
+
+        getContext().deleteDatabase("CreateIndexDb");
+
+        db = DbQuery.getDatabase("CreateIndexDb");
+        db.open(1, new SQLiteBuilder() {
+            @Override
+            public Context getContext() {
+                return CreateIndexTest.this.getContext();
+            }
+
+            @Override
+            public void onModelCreate(IDatabase database, IDatabase.Modeling modeling) {
+                modeling.add("Person")
+                        .addPrimaryKey("Id")
+                        .add("Name", "String")
+                        .add("Age", "Integer")
+                        .add("Address", "Blob")
+                        .index("Id");
+            }
+        });
+
+        db.get("Person").deleteAll();
+    }
+}
