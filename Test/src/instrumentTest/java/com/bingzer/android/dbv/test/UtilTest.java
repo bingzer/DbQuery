f155ff6184d8ff114682b827ee8599f999c58813
bingzer <ricky@bingzer.com>
2013-08-15 15:01:44 -0400

    bindArgs tests

--- Test/src/instrumentTest/java/com/bingzer/android/dbv/test/UtilTest.java
+++ Test/src/instrumentTest/java/com/bingzer/android/dbv/test/UtilTest.java
@@ -9,7 +9,7 @@ import com.bingzer.android.dbv.Util;
  */
 public class UtilTest extends AndroidTestCase{
 
-    public void test_safeEscape(){
+    public void testSafeEscape(){
         String bad = "'hello'";
         String good = "hello";
 
@@ -18,5 +18,22 @@ public class UtilTest extends AndroidTestCase{
 
         assertTrue(bad.equalsIgnoreCase("'''hello'''"));
         assertTrue(good.equalsIgnoreCase("'hello'"));
+    }
+
+    public void testJoin(){
+        assertTrue(Util.join(",","a","b").equals("a,b"));
+        assertTrue(Util.join(".","a","b").equals("a.b"));
+    }
+
+    public void testBindArgs(){
+        assertTrue(Util.bindArgs("ENTER THE DARK SIDE", 1,2,3).equals("ENTER THE DARK SIDE"));
+        assertTrue(Util.bindArgs("Hello ?, How ? You", "World", 'R', "Invalid").equals("Hello 'World', How 'R' You"));
+        assertTrue(Util.bindArgs("INSERT INTO (?,?,?)", 1,2,3).equals("INSERT INTO (1,2,3)"));
+
+        assertTrue(Util.bindArgs("?", "'wake'").equals("'''wake'''"));
+    }
+
+    public void testBindArgs_WithMissingArgs(){
+        assertTrue(Util.bindArgs("Hello ?, how are you ?", "World").equals("Hello 'World', how are you ?"));
     }
 }
